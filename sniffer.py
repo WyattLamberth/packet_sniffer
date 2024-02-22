3c13ec79a25f03049ecee5b08f051bb1d02b8053
Wyatt <wyattlamberth@outlook.com>
2024-02-22 16:33:11 -0600

    Update sniffer.py to use scapy instead of socket...

--- sniffer.py
+++ sniffer.py
@@ -1,77 +1,35 @@
-import socket
-import os
-import struct
-from ctypes import *
+from scapy.all import sniff, IP, TCP
 
-# host to listen on
-host = "YOUR_HOST_IP_HERE"
+# Define the packet processing callback
+def process_packet(packet):
+    # Check for IP layer
+    if IP in packet:
+        ip_src = packet[IP].src
+        ip_dst = packet[IP].dst
+        print(f"IP Packet: {ip_src} -> {ip_dst}")
 
-# our IP header
-class IP(Structure):
-    _fields_ = [
-        ("ihl", c_ubyte, 4),
-        ("version", c_ubyte, 4),
-        ("tos", c_ubyte),
-        ("len", c_ushort),
-        ("id", c_ushort),
-        ("offset", c_ushort),
-        ("ttl", c_ubyte),
-        ("protocol_num", c_ubyte),
-        ("sum", c_ushort),
-        ("src", c_uint32),
-        ("dst", c_uint32),
-    ]
+        # Check for TCP layer
+        if TCP in packet:
+            tcp_sport = packet[TCP].sport
+            tcp_dport = packet[TCP].dport
+            print(f"TCP Packet: Src Port: {tcp_sport}, Dst Port: {tcp_dport}")
 
-    def __new__(cls, socket_buffer=None):
-        return cls.from_buffer_copy(socket_buffer)
+            # Check for payload (data)
+            if packet[TCP].payload:
+                payload = packet[TCP].load
+                try:
+                    # Attempt to decode payload
+                    decoded_payload = payload.decode('utf-8')
+                    print(f"Payload: {decoded_payload}")
+                except UnicodeDecodeError:
+                    # Payload might not be text-based; print a hexdump for analysis
+                    print("Payload contains non-text data.")
+                    packet[TCP].payload.show()
 
-    def __init__(self, socket_buffer=None):
-        # map protocol constants to their names
-        self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
-
-        # human readable IP addresses
-        self.src_address = socket.inet_ntoa(struct.pack("<L", self.src))
-        self.dst_address = socket.inet_ntoa(struct.pack("<L", self.dst))
-
-        # human readable protocol
-        try:
-            self.protocol = self.protocol_map[self.protocol_num]
-        except:
-            self.protocol = str(self.protocol_num)
-
-def sniff():
-    if os.name == "nt":
-        socket_protocol = socket.IPPROTO_IP
-    else:
-        socket_protocol = socket.IPPROTO_ICMP
-
-    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
-    sniffer.bind((host, 0))
-
-    # we want the IP headers included in the capture
-    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-
-    # if we're using Windows, we need to send an IOCTL
-    # to set up promiscuous mode
-    if os.name == "nt":
-        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-
-    try:
-        while True:
-            # read in a packet
-            raw_buffer = sniffer.recvfrom(65565)[0]
-
-            # create an IP header from the first 20 bytes of the buffer
-            ip_header = IP(raw_buffer[0:20])
-
-            # print out the protocol that was detected and the hosts
-            print(f"Protocol: {ip_header.protocol} {ip_header.src_address} -> {ip_header.dst_address}")
-
-    # handle CTRL-C
-    except KeyboardInterrupt:
-        # if we're using Windows, turn off promiscuous mode
-        if os.name == "nt":
-            sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+def main():
+    print("Starting packet capture... Press CTRL+C to stop.")
+    # Start sniffing packets
+    sniff(prn=process_packet, store=False)
 
 if __name__ == "__main__":
-    sniff()
+    main()
